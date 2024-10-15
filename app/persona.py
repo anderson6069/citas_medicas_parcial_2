989d05be30ea10b00b646403d8248a12ff9258c2
anderson6069 <134990473+anderson6069@users.noreply.github.com>
2024-10-14 23:56:22 -0500

    2

--- app/persona.py
+++ app/persona.py
@@ -3,3 +3,19 @@ class Persona:
         self.identificacion = identificacion
         self.nombre = nombre
         self.celular = celular
+
+
+class Medico(Persona):
+    def __init__(self, identificacion, nombre, celular, especialidad):
+        super().__init__(identificacion, nombre, celular)
+        self.especialidad = especialidad
+
+
+class Paciente(Persona):
+    def __init__(self, identificacion, nombre, celular, correo):
+        super().__init__(identificacion, nombre, celular)
+        self.correo = correo
+        self.medico_preferencia = None
+
+    def asignar_medico_preferencia(self, medico):
+        self.medico_preferencia = medico
--- app/persona_factory.py
+++ app/persona_factory.py
@@ -1,15 +1,12 @@
-from paciente import Paciente
-from medico import Medico
+from persona import Medico, Paciente
 
 
 class PersonasFactory:
     @staticmethod
     def crear_persona(tipo, identificacion, nombre, celular, especialidad=None, correo=None):
-        if tipo.lower() == 'medico':
+        if tipo == "medico":
             return Medico(identificacion, nombre, celular, especialidad)
-
-        elif tipo.lower() == 'paciente':
+        elif tipo == "paciente":
             return Paciente(identificacion, nombre, celular, correo)
-
         else:
-            raise ValueError(f"Tipo de persona desconocido: {tipo}")
+            raise ValueError("Tipo de persona inválido.")
